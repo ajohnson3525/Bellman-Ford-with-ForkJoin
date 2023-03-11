9f72793dc50f77abd40b5c24cbcb5fd41f50408a
AJ <aej3525@uw.edu>
2023-03-10 22:03:16 -0800

    began InParallel

--- src/main/java/paralleltasks/RelaxInTask.java
+++ src/main/java/paralleltasks/RelaxInTask.java
@@ -1,7 +1,10 @@
 package paralleltasks;
 
 import cse332.exceptions.NotYetImplementedException;
+import cse332.graph.GraphUtil;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
@@ -9,22 +12,54 @@ public class RelaxInTask extends RecursiveAction {
 
     public static final ForkJoinPool pool = new ForkJoinPool();
     public static final int CUTOFF = 1;
-    final int lo, hi;
+    //final int lo, hi;
+    private static int lo, hi;
+    private static int[] D1, D2, P;
+    private static List<HashMap<Integer, Integer>> g;
 
-    public RelaxInTask() {
-        throw new NotYetImplementedException();
+    public RelaxInTask(List<HashMap<Integer, Integer>> g, int[] D1, int[] D2, int [] P,
+                       int lo, int hi) {
+
+        this.g = g;
+        this.D1 = D1;
+        this.D2 = D2;
+        this.P = P;
+        this.lo = lo;
+        this.hi = hi;
+
     }
 
     protected void compute() {
-        throw new NotYetImplementedException();
+        if (hi - lo <= CUTOFF) {
+            //sequential
+            sequential();
+        } else {
+            // parallel
+            RelaxInTask left = new RelaxInTask(g, D1, D2, P, lo, (hi + lo) / 2);
+            RelaxInTask right = new RelaxInTask(g, D1, D2, P, (hi + lo) / 2, hi);
+            left.fork();
+            right.compute();
+            left.join();
+        }
     }
 
     public static void sequential() {
-        throw new NotYetImplementedException();
+        int cost;
+        for (int w = lo; w < hi; w++) {
+            for (int v : g.get(w).keySet()) {
+                cost = g.get(w).get(v); // cost from v to w
+                if (D2[v] != GraphUtil.INF && D1[w] > D2[v] + cost) {
+                    D1[w] = D2[v] + cost;
+                    P[w] = cost;
+                }
+            }
+        }
     }
 
-    public static void parallel() {
-        throw new NotYetImplementedException();
+    public static void parallel(List<HashMap<Integer, Integer>> g, int[] D1, int[] D2,
+                                int[] P, int n) {
+        pool.invoke(new RelaxInTask(g, D1, D2, P, 0, n));
+
     }
 
 }
--- src/main/java/solvers/InParallel.java
+++ src/main/java/solvers/InParallel.java
@@ -1,17 +1,44 @@
 package solvers;
 
 import cse332.exceptions.NotYetImplementedException;
+import cse332.graph.GraphUtil;
 import cse332.interfaces.BellmanFordSolver;
 import main.Parser;
+import paralleltasks.ArrayCopyTask;
+import paralleltasks.RelaxInTask;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class InParallel implements BellmanFordSolver {
 
     public List<Integer> solve(int[][] adjMatrix, int source) {
-        Object g = Parser.parseInverse(adjMatrix);
+        List<HashMap<Integer, Integer>> g = Parser.parseInverse(adjMatrix);
+        int n = adjMatrix.length;
+        int[] D1 = new int[n];
+        int[] D2 = new int[n];
+        int[] P = new int[n];
+        int cost;
 
-        throw new NotYetImplementedException();
+        // initialize D1 & P
+        for (int i = 0; i < n; i++) {
+            P[i] = -1;
+            if (i == source) {
+                D1[i] = 0;
+            } else {
+                D1[i] = GraphUtil.INF;
+            }
+        }
+
+        // Bellman-Ford algorithm
+        for (int k = 0; k < n; k++) {
+            // Array copying
+            D2 = ArrayCopyTask.copy(D1);
+            // Relaxing the edges
+            RelaxInTask.parallel(g, D1, D2, P, n);
+        }
+
+        return GraphUtil.getCycle(P);
     }
 
 }
